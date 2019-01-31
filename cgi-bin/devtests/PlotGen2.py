b39b383576689dc283effc1f04f2289f535b5fc3
Joe Ranalli <jar339@psu.edu>
2019-01-31 15:38:12 -0500

    PlotGen2 PDF demo

--- cgi-bin/devtests/PlotGen2.py
+++ cgi-bin/devtests/PlotGen2.py
@@ -6,7 +6,7 @@ R134 = pm.get('mp.C2H2F4')
 
 pm.config['unit_pressure'] = 'MPa'
 sz = (11,8.25)
-pyroplot.Ts(water,plines = None,size=sz)
+#pyroplot.Ts(water,plines = None,size=sz)
 #pyroplot.Ts(R134,size=sz)
 #pyroplot.Tv(water,size=sz)
 #pyroplot.Tv(R134,size=sz)
@@ -17,3 +17,14 @@ pyroplot.Ts(water,plines = None,size=sz)
 #ax = pyroplot.Ts(water,size=sz,display=False)
 #f = ax.get_figure()
 #f.show()
+
+#generate PDF
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('test.pdf')
+ax1=pyroplot.Ts(water,size=sz,display=False)
+ax2=pyroplot.Tv(water,size=sz,display=False)
+ax3=pyroplot.pv(water,size=sz,display=False)
+pp.savefig(ax1.get_figure())
+pp.savefig(ax2.get_figure())
+pp.savefig(ax3.get_figure())
+pp.close()
