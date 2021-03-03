9103e19641eb2a01bf5a31a06f927316e5e85493
Chris Martin <crm28@psu.edu>
2021-03-02 23:12:03 -0500

    Drafted new iteration pdf and started writing an EOS pdf (instead of poly)

--- poly2/benchmark.py
+++ poly2/benchmark.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
 # power benchmark
 
 import timeit
 import numpy as np
 import matplotlib.pyplot as plt
+import os,sys
 
 def testeval(test, N=1, Navg=1000, ca=1., cb=1.):
     setupstr = 'import numpy as np;'
@@ -10,22 +12,54 @@ def testeval(test, N=1, Navg=1000, ca=1., cb=1.):
     setupstr += 'b = %f * np.random.random(%d)'%(cb,N)
     TT = timeit.Timer(test, setup=setupstr)
     return TT.timeit(number=Navg)
-    
-Navg=100000
-N = [1,2,5,10,20,50,100,200,500,1000,2000,5000,10000]
-print 'a**b...'
-t_star = [testeval('a**b',n,Navg=Navg)/Navg for n in N]
-print 'a*b...'
-t_mul = [testeval('a*b',n,Navg=Navg)/Navg for n in N]
-print 'power(a,b)...'
-t_pow = [testeval('np.power(a,b)',n,Navg=Navg)/Navg for n in N]
 
-f = plt.figure(1)
-f.clf()
-ax = f.add_subplot(111)
-ax.loglog(N,t_star,'bo',label='**')
-ax.loglog(N,t_mul,'go',label='*')
-ax.loglog(N,t_pow,'ro',label='np.power')
-ax.legend(loc=0)
-ax.grid('on')
-f.savefig('benchmark.png')
+
+with open('benchmark.dat','w') as ff:
+    ff.write(repr(os.uname()))
+
+    Navg=20000
+    N = [1,2,5,10,20,50,100,200,500,1000,2000,5000,10000]
+
+    ff.write('Navg = %d\n'%Navg)
+    ff.write('# Time tables are in the format \n#n:t\nWhere n is the number of array elements\n# and t is the average execution time in seconds\n')
+
+    # Start with a ** b
+    ff.write('\n\n# a ** b\n')
+    sys.stdout.write('a**b\n')
+    t_star = []
+    for n in N:
+        t = testeval('a**b',n,Navg=Navg)/Navg
+        t_star.append(t)
+        sys.stdout.write('%d:%f\n'%(n,t))
+        ff.write('%d:%f\n'%(n,t))
+
+    ff.write('\n\n# a * b\n')
+    sys.stdout.write('a*b\n')
+    t_mul = []
+    for n in N:
+        t = testeval('a*b',n,Navg=Navg)/Navg
+        t_mul.append(t)
+        sys.stdout.write('%d:%f\n'%(n,t))
+        ff.write('%d:%f\n'%(n,t))
+
+    ff.write('\n\n# np.power(a, b)\n')
+    sys.stdout.write('np.power(a,b)\n')
+    t_pow = []
+    for n in N:
+        t = testeval('np.power(a,b)',n,Navg=Navg)/Navg
+        t_pow.append(t)
+        sys.stdout.write('%d:%f\n'%(n,t))
+        ff.write('%d:%f\n'%(n,t))
+
+
+    f = plt.figure(1)
+    f.clf()
+    ax = f.add_subplot(111)
+    ax.loglog(N,t_star,'bo',label='**')
+    ax.loglog(N,t_mul,'go',label='*')
+    ax.loglog(N,t_pow,'ro',label='np.power')
+    ax.legend(loc=0)
+    ax.grid('on')
+    ax.set_xlabel('Array size')
+    ax.set_ylabel('Mean execution time (sec)')
+    f.savefig('benchmark.png')
