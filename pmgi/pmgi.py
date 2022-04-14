dd3cf3d3d24df773ca162b6490c4bbd6d630669a
Joe Ranalli <jar339@psu.edu>
2022-04-14 15:46:52 -0400

    Lots of changes.
    Can handle GET and POST on property requests, based on a JSON format for passing data. Uses substance.state() for property calcs. Uses JSON for data passing. Can handle units in a very rudimentary way.

--- pmgi/pmgi.py
+++ pmgi/pmgi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import flask
 import pyromat as pm
 import numpy as np
 from flask import __version__ as flaskv
@@ -11,23 +11,30 @@ __version__ = '0.0'
 
 #### Helper functions
 def toarray(a):
-    return np.asarray(a.split(','), dtype=float)
+    try:
+        return np.asarray(a, dtype=float)
+    except ValueError:
+        return np.asarray(a.split(','), dtype=float)
 
-def clean(a):
-    for name, value in a.items():
+
+def json_friendly(dictdata):
+    """
+    Clean up a dictionary for output as JSON.
+    """
+    for name, value in dictdata.items():
         if isinstance(value, dict):
-            clean(value)
+            json_friendly(value)
         elif isinstance(value, np.ndarray):
             if value.size == 1:
-                a[name] = np.asscalar(value)
+                dictdata[name] = np.asscalar(value)
             else:
-                a[name] = value.tolist()
+                dictdata[name] = value.tolist()
+
+
 ###
 # Custom request processing classes
 #   These are designed to construct a JSON dictionary that will be used
 #   by the HTML/JS on the client side to construct tables and plots.
-#   
-# Still to do... extend the arguments/return values to tolerate arrays
 ###
 
 
@@ -39,9 +46,8 @@ class PMGIRequest:
         self.substance = None
         self.out = {'error':False, 'message':''}
 
-    
-    def getid(self, idstr):
-        """GETID - Retrieve a PYroMat object and fail gracefully
+    def init_subst(self, idstr):
+        """INIT_SUBST - Retrieve a PYroMat object and fail gracefully
             getid(idstr)
 
         Stores a PYroMat substance instance in the "s" member if successful.
@@ -80,16 +86,16 @@ class PMGIRequest:
         """
         mandatory = set(mandatory)
         # Loop through the items
-        for name,value in self.args.items():
+        for name, value in self.args.items():
             # Is this a recognized argument?
             if name not in types:
-                self.error(f'Unrecognized argument: {name}\n')
+                self.error(f'Unrecognized argument: {name}')
                 return True
             # If the argument is known
             try:
-                self.args[name] = types[name](value)
+                self.args[name] = types[name](value)  # Cast to type
             except:
-                self.error(f'Invalid argument: {name}={value}\n')
+                self.error(f'Invalid argument: {name}={value}')
                 return True
             # If the argument is mandatory, check it off the list
             if name in mandatory:
@@ -99,21 +105,32 @@ class PMGIRequest:
             self.error(f'Missing mandatory arguments:')
             prefix = ' '
             for name in mandatory:
-                self.warn(prefix + name)
+                self.warn(prefix + name, append_newline=False)
                 prefix = ', '
-            self.warn('\n')
+            self.warn('')
             return True
         return False
 
-    def error(self, message):
+    def error(self, message, append_newline=True):
+        """
+        Set flag for error and append an error message.
+        """
         self.out['error'] = True
         self.out['message'] += message
+        if append_newline:
+            self.out['message'] += "\n"
 
-    def warn(self, message):
+    def warn(self, message, append_newline=True):
+        """
+        Append warning messages.
+        """
         self.out['message'] += message
+        if append_newline:
+            self.out['message'] += "\n"
+
 
 class PropertyRequest(PMGIRequest):
-    def __init__(self, args):
+    def __init__(self, args, units=None):
         # Clean initialization
         PMGIRequest.__init__(self)
 
@@ -134,10 +151,16 @@ class PropertyRequest(PMGIRequest):
                 mandatory=['id']):
             return
 
-        # Retrieve the PM entry
-        if self.getid(self.args['id']):
-            return
+        # Config the units
+        if units is not None:
+            try:
+                UnitHandler.set_units(units)
+            except pm.utility.PMParamError as e:
+                self.error(e.args[0])
 
+        # Retrieve the PM entry
+        if self.init_subst(self.args['id']):
+            return
 
     def process(self):
         """Process the request
@@ -148,7 +171,7 @@ class PropertyRequest(PMGIRequest):
         if self.out['error']:
             return
         elif not isinstance(self.substance, pm.reg.__basedata__):
-            self.error('Substance data seems to be corrupt!  Halting.\n')
+            self.error('Substance data seems to be corrupt!  Halting.')
             return
 
         #
@@ -159,16 +182,14 @@ class PropertyRequest(PMGIRequest):
         elif self.substance.data['id'].startswith('mp'):
             self._mp_process()
         else:
-            self.error(f'Could not determine the collection for substance: {self.substance.data["id"]}\n')
+            self.error(f'Could not determine the collection for substance: {self.substance.data["id"]}')
             return
 
         # Finally, clean up the return parameters
-        clean(self.out)
-
+        json_friendly(self.out)
 
     def _ig_process(self):
-        self.error('THE IG PROCESS IS NOT WRITTEN YET!\nSorry for shouting\n')
-
+        self.error('THE IG PROCESS IS NOT WRITTEN YET!\nSorry for shouting')
 
     def _mp_process(self):
         """_mp_process
@@ -183,16 +204,58 @@ class PropertyRequest(PMGIRequest):
 
         try:
             self.out['data'] = self.substance.state(**args)
-        except pm.utility.PMParamError as e:
+        except (pm.utility.PMParamError, pm.utility.PMAnalysisError) as e:
+            # Add in the error response from pyromat
             self.error(e.args[0])
-            self.error(' Args found:')
+            self.error('Args found:', append_newline=False)
             prefix = '  '
             for name in args:
-                self.warn(prefix + name)
+                self.warn(prefix + name, append_newline=False)
                 prefix = ', '
+            self.warn('')
             return
 
 
+class UnitHandler:
+    _valid_unit_strs = ['energy', 'force', 'length', 'mass', 'molar', 'pressure', 'temperature', 'time', 'volume']
+
+    @staticmethod
+    def set_units(units):
+        units = dict(units)
+
+        for name, value in units.items():
+            if name in UnitHandler._valid_unit_strs and \
+                    value in UnitHandler.list_valid_units(name):
+                pm.config['unit_'+name] = value
+            else:
+                raise pm.utility.PMParamError("Invalid unit specification:"
+                                              f"{name}={value}")
+
+
+    @staticmethod
+    def get_units():
+        out = {}
+        cfg = pm.config
+        for key in cfg.entries:
+            if key.startswith('unit'):
+                out[key.split('_')[1]] = cfg[key]
+        return out
+
+    @staticmethod
+    def list_valid_units(units=None):
+        out = {}
+
+        if units is None:
+            units = UnitHandler._valid_unit_strs
+
+        if type(units) is str:
+            unitfun = getattr(pm.units, units)
+            out = list(unitfun.get())
+        else:
+            for unit in units:
+                unitfun = getattr(pm.units, unit)
+                out[unit] = list(unitfun.get())
+        return out
 
 
 ############################
@@ -204,18 +267,24 @@ app = Flask(__name__)
 # will redirect here - to root.
 #
 # The root pmgi accepts property requests.
+
 @app.route('/', methods=['POST', 'GET'])
 def pmgi():
-    out = {'message':''}
-    args = {}
     # Read in the request data to an args dict
     if request.method == 'POST':
-        args = dict(request.form)
+        jsondat = dict(request.json)
+        args = jsondat['state_input']
+        units = None
+        if 'units' in jsondat:
+            units = (jsondat['units'])
+        pr = PropertyRequest(args, units)
     elif request.method == 'GET':
         args = dict(request.args)
+        pr = PropertyRequest(args)
 
-    pr = PropertyRequest(args)
     pr.process()
+
+    # Check for error and return code
     if pr.out['error']:
         return pr.out, 500
     else:
@@ -231,10 +300,12 @@ def get():
         pass
     return out
 
+
 # The info pmgi will return the results of queries (e.g. substance search)
 @app.route('/info', methods=['POST', 'GET'])
 def info():
-    return {}
+    return {'units': UnitHandler.get_units(),
+            'valid_units': UnitHandler.list_valid_units()}
 
 # Version is responsible for returning basic system information
 # This will be important if users want to diagnose differences between
@@ -249,6 +320,17 @@ def meta():
         'numpy':np.version.full_version,
     }
     return out
+
+
+# ##### DELETE ME FOR DEPLOY - ROUTE FOR SERVING STATIC HTML DURING DEV:
+# ##### USE CASE - navigate to http://127.0.0.1:5000/index to browse page at:
+# ##### /static/index.html
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    if not app.debug:
+        flask.abort(404)
+    return app.send_static_file('%s.html' % page_name)
+
 
 if __name__ == '__main__':
     app.run()
--- pmgi/static/script.js
+++ pmgi/static/script.js
@@ -1,50 +1,141 @@
+// Execute when the page loads
+$(document).ready(function(){
 
-function getprops() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText);
-            var table = document.getElementById("proptable");
-            var row = table.insertRow(table.rows.length-1);
-            row.insertCell(0).innerHTML = data.T;
-            row.insertCell(1).innerHTML = data.p;
-            row.insertCell(2).innerHTML = data.d;
-            row.insertCell(3).innerHTML = data.h;
-            row.insertCell(4).innerHTML = data.s;
+});
+
+
+function popup() {
+    // TODO - make this a units edit place
+   var popwindow = document.getElementById("checkBundle");
+   if (popwindow.style.display === "none") {
+       popwindow.style.display = "block";
+  } else {
+        popwindow.style.display = "none";
+    }
+};
+
+
+/**
+ * Convert a form object into JSON that is suitable for passing to PMGI.
+ *
+ * Form elements must have a "name" field. The keys of the JSON will be
+ * shortened using only the first letter of the name. In order to be useful
+ * with PMGI, the form element names should all use their PMGI property
+ * identifier as their first letter.
+ *
+ * @param formID the Element ID of the form
+ * @returns Object, keyed by thermodynamic property, with values as floats.
+ */
+function propFormToJSON(formID, append){
+
+    let outdata = {};
+    let data = Object.fromEntries(new FormData(document.getElementById(formID)));
+    for (const key in data) {
+        let shortkey = key[0];
+        let v = parseFloat(data[key]);
+        if (v){
+            outdata[shortkey] = v;
         }
     };
-    var requeststr = "http://omnifariousbox.com/pmgi/?id=mp.H2O";
-    requeststr += "&T=" + document.getElementById("Tinput").value;
-    requeststr += "&p=" + document.getElementById("pinput").value;
-    xhttp.open("GET", requeststr, true);
-    xhttp.send();
+
+    if (append !== undefined){
+        for (const key in append){
+            outdata[key] = append[key];
+        }
+    }
+    return outdata;
 }
 
 
-function updateinputs() {
-    // First, count the number of nonzero entries
-    var count;
-    var inputs = [document.getElementById("Tinput"), document.getElementById("pinput"), document.getElementById("dinput"), document.getElementById("hinput"), document.getElementById("sinput")];
-    var x;
-    
-    count = 0;
-    for (x of inputs){
-        if (x.value.length){
-            count += 1;
-        }
-    }
-    
-    if(count < 2){
-        for (x of inputs){
-            x.disabled = false;
-        }
-    }else{
-        for (x of inputs){
-            if (x.value.length==0){
-                x.disabled = true;
-            }
-        }
-    }
-    
-    // Then, grey out the entries that are zero (if appropriate)
+function buildTable(data){
+    var table = document.getElementById("proptable");
+    var row = table.insertRow(table.rows.length-1);
+    row.insertCell(0).innerHTML = data.T;
+    row.insertCell(1).innerHTML = data.p;
+    row.insertCell(2).innerHTML = data.d;
+    row.insertCell(3).innerHTML = data.h;
+    row.insertCell(4).innerHTML = data.s;
 }
+
+
+
+function propResponseSuccess(data){
+    // TODO generalize!!
+    buildTable(data.data);
+}
+
+function propResponseFail(data){
+    // TODO better error handling?
+    try {
+       let resp = JSON.parse(data.responseText);
+       alert(resp.message);
+    } catch (error) {
+       alert("An unhandled error occurred: " + data.responseText);
+    }
+}
+
+
+function postProps(){
+    let requestroute = "/";
+    let formData = propFormToJSON('propform', {'id': 'mp.H2O'});
+    let unitFormData = Object.fromEntries(new FormData(document.getElementById("unitform")));
+    let postData = {state_input: formData, units: unitFormData};
+    // Forced to operate as $.ajax because we need to specify that we're
+    // passing json.
+    $.ajax({
+        url: requestroute,
+        type: "POST",
+        data: JSON.stringify(postData),
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: propResponseSuccess,
+        error: propResponseFail
+    });
+}
+
+
+function getProps(){
+    let requestroute = "/";
+    let formData = propFormToJSON('propform', {'id': 'mp.H2O'});
+   $.get(requestroute, formData, propResponseSuccess,dataType='json')
+    .fail(propResponseFail);
+}
+
+
+function populate_list() {
+    $.getJSON("/Admin/GetFolderList/", function(result) {
+        var $dropdown = $("#dropdown");
+        $.each(result, function() {
+            $dropdown.append($("<option />").val(this.ImageFolderID).text(this.Name));
+        });
+    });
+}
+
+//
+// function updateinputs() {
+//     // First, count the number of nonzero entries
+//     var count;
+//     var inputs = [document.getElementById("Tinput"), document.getElementById("pinput"), document.getElementById("dinput"), document.getElementById("hinput"), document.getElementById("sinput")];
+//     var x;
+//
+//     count = 0;
+//     for (x of inputs){
+//         if (x.value.length){
+//             count += 1;
+//         }
+//     }
+//
+//     if(count < 2){
+//         for (x of inputs){
+//             x.disabled = false;
+//         }
+//     }else{
+//         for (x of inputs){
+//             if (x.value.length==0){
+//                 x.disabled = true;
+//             }
+//         }
+//     }
+//
+//     // Then, grey out the entries that are zero (if appropriate)
+// }
